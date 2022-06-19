bc25e613531f75edb3a505ff7cbf10df1e9fb5e6
Your Name <you@example.com>
2022-06-18 23:34:13 -0300

    criando context

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+import { Fragment } from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+  import Home from "../pages/Home";
+  import Signin from "../pages/Signin";
+  import Signup from "../pages/Signup";
+
+const Private = ({ Item }) => {
+
+    const signed = true;
+    
+    return signed > 0 ? <Item /> : <Signin />;
+}
+
+const Routesapp = () => {
+    return (
+        <BrowserRouter>
+        <Fragment>
+            <Routes>
+                <Route exact path="/home" element={<Private Item={Home} />} />
+                <Route path="/" element={<Signin />} />
+                <Route exact path="/signup" element={<Signup />} />
+                <Route path="*" element={<Signin />} />
+            </Routes>
+        </Fragment>
+        </BrowserRouter>
+    )
+}
+
+export default Routesapp;
